5fcd87e94d1b52c8283ab98772045d9a9c84a409
Jenice15 <jenicemartin15@gmail.com>
2024-06-02 15:54:04 -0700

    added function for search

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchEngine.css';
 
 export default function SearchEngine() {
+    const [keyword, setKeyword] = useState('');
+
+    function searchKeyword(event) {
+        event.preventDefault();
+        alert(`searching for ${keyword}.`);
+    }
+
+    function handleKeyword(event) {
+        setKeyword(event.target.value);
+    }
+
     return (
         <div className='SearchEngine open-sans-searchEngine'>
             <p className='SearchBarTag'>Type your Word</p>
-            <form className='Form'>
+            <form className='Form' onSubmit={searchKeyword}>
                 <input
                     type='searchBar'
                     placeholder='For Example: Phlegmatic '
                     className='SearchBar'
+                    onChange={handleKeyword}
                 />
                 <br />
                 <button className='SearchButton btn '>Search</button>
