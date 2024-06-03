4708c3a5a42bd0ef84ea8ee2e25610b860213ed7
Jenice15 <jenicemartin15@gmail.com>
2024-06-02 22:50:53 -0700

    added api with axios and made ajax call for the search function

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,12 +1,24 @@
 import React, { useState } from 'react';
 import './SearchEngine.css';
+import axios from 'axios';
 
 export default function SearchEngine() {
     const [keyword, setKeyword] = useState('');
+    // const [wordMeaning, setWordMeaning] = useState('');
+
+    function displayDictionary(response) {
+        // setWordMeaning(response.data);
+        console.log(response);
+    }
 
     function searchKeyword(event) {
         event.preventDefault();
         alert(`searching for ${keyword}.`);
+
+        let apiKey = 'fa90t5bf5523344e459f280fabbb9o83';
+        let word = keyword;
+        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}`;
+        axios.get(apiUrl).then(displayDictionary);
     }
 
     function handleKeyword(event) {
